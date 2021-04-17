4e7093716bc83099ec08685c3eb53f478b2eb3cf
KJSweeden <kieransweeden@gmail.com>
2021-04-17 09:33:36 +0100

    Included single get, post, put & delete requests

--- app.js
+++ app.js
@@ -1,11 +1,64 @@
 const http = new easyHTTP;
 
 // Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts){
+// http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts){
+
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+// Get Single Post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function (err, posts){
+
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+// Create Data (Using names from JSON file from API) - The REST API adds an ID for us
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post'
+};
+
+// Create Post (Look at documentation - same as get, but it's okay as we're using a different method)
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post){
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Update post (put) (notice the /1 at the end of url, we're updating a post at ID index 1)
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post){
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Updating post at position 5
+// http.put('https://jsonplaceholder.typicode.com/posts/5', data, function(err, post){
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Delete post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function (err, response){
 
   if (err) {
     console.log(err);
   } else {
-    console.log(posts);
+    console.log(response);
   }
 });
--- easyhttp.js
+++ easyhttp.js
@@ -24,7 +24,57 @@ easyHTTP.prototype.get = function(url, callback) {
 }
 
 // Make a HTTP POST REQUEST
+easyHTTP.prototype.post = function(url, data, callback) {
+  this.http.open('POST', url, true);
+
+  // Setting the content type by setting a name of the header whose value is to be set.
+  // setRequestHeader (name, value)
+  this.http.setRequestHeader('Content-type', 'application/json');
+  
+  let self = this;
+  this.http.onload = function () {
+    // Doesn't need status as we're doing a post request
+
+    // The responseText should be the new post that we added with the ID of 101
+    callback (null, self.http.responseText);
+  }
+
+  // Data will be an object, so it's important to stringify (transform to JSON) the data in order to send it to the API
+  this.http.send(JSON.stringify(data));
+}
 
 // Make a HTTP PUT REQUEST
+easyHTTP.prototype.put = function(url, data, callback) {
+  this.http.open('PUT', url, true);
 
+  // Setting the content type by setting a name of the header whose value is to be set.
+  // setRequestHeader (name, value)
+  this.http.setRequestHeader('Content-type', 'application/json');
+  
+  let self = this;
+  this.http.onload = function () {
+    // Doesn't need status as we're doing a put request
+
+    // The responseText should be the new post that we added with the ID of 101 (automatically added by API)
+    callback (null, self.http.responseText);
+  }
+
+  this.http.send(JSON.stringify(data));
+}
 // Make a HTTP DELETE REQUEST
+easyHTTP.prototype.delete = function(url, callback) {
+  this.http.open('DELETE', url, true);
+
+  let self = this;
+  this.http.onload = function () {
+    // Do check status for delete
+    if (self.http.status === 200) {
+      // responseText is going to be empty as we're using the delete method - so instead we're going to inform ourselves that the post has been deleted
+      callback (null, 'Post Deleted');
+    } else {
+      callback ('Error: ' + self.http.status);
+    }
+  }
+
+  this.http.send();
+}
